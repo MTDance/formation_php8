bca7823ff41abdfad3003ee7c49d0a51a7a7e358
TRAN <minhtuan46@hotmail.fr>
2022-09-28 17:26:50 +0200

    exercice 2 php8

---
+++ 2_manipuler_les_chaine_de_caractere/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $chaine = "Ceci est une chaîne de caractères";
+
+        // Afficher 1 caractère de la chaine
+        // echo $chaine[5];
+
+        // Modifier un caractère de la chaine
+        // $chaine [0]= "F";
+
+        // Extraire une partie de la chaine
+        // var_dump(substr($chaine, -11)) ;
+
+        // Remplacer une partie de la chaîne
+        $chaine = str_replace('Ceci', 'Celà',$chaine);
+
+        var_dump(strpos($chaine, "Le"));
+    ?>
+</body>
+</html>
